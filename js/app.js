03c55f768b29acafe216499273f4d166c3748364
Michael Weber <webermn15@gmail.com>
2017-12-26 12:04:05 -0600

    improved mountain drawing function

--- js/app.js
+++ js/app.js
@@ -10,27 +10,40 @@ function draw() {
 	if (canvas.getContext) {
 		let ctx = canvas.getContext('2d');
 
+		let iStrokeWidth = 1;
+		let iTranslate = (iStrokeWidth % 2) / 2;
+		ctx.translate(iTranslate, iTranslate);
+
 		let slopeChange = 2;
+		let maxSlope = 10;
+		let slope = Math.random() * slopeChange;
 		let maxHeight = canvas.height;
 		let height = 300;
+		let valley = 450;
+		let peak = 200;
+
 
 		for (let i = 0; i < canvas.width; i++) {
-			let randomChange = Math.random();
-			if (randomChange > 0.5) {
-				height += slopeChange;
+			slope += Math.random() * slopeChange;
+			console.log(slope);
+			if (slope > maxSlope) {
+				slope = 1;
 			}
-			else {
-				height -= slopeChange;
+			if (height < valley) {
+				height += slope;
+				slopeChange *= -1;
+			}
+			else if (height > peak) {
+				height -= slope;
+				slopeChange *= -1;
 			}
 
-			console.log(i, height);
 
 			ctx.beginPath();
 			ctx.moveTo(i, maxHeight);
 			ctx.lineTo(i, height);
 			ctx.stroke();
 		}
-
 
 	}
 		
