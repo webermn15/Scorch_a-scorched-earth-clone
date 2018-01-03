8bc323810182b76175096fd1fa7cc362dc55e6db
Michael Weber <webermn15@gmail.com>
2018-01-03 11:35:41 -0600

    started moving stuff into a nice clean game object, added buttons that will allow the player to initialize the game with a selected number of players (temporary but nice for now)

--- js/app.js
+++ js/app.js
@@ -11,73 +11,178 @@ const scorch = () => {
 	const playerDisplay = document.getElementById('turn');
 	const powerDisplay = document.getElementById('power');
 	const angleDisplay = document.getElementById('angle');
+	const startButton = document.getElementById('START-ER-UP');
 
 	//canvas
-	let canvas = document.getElementById('game-board');
-	let ctx = canvas.getContext('2d');
-
+	const canvas = document.getElementById('game-board');
+	const ctx = canvas.getContext('2d');
 
 	//canvas background separate from the other one for collision purposes
-	let canvasBackground = document.getElementById('background');
-	let context = canvasBackground.getContext('2d');
+	const canvasBackground = document.getElementById('background');
+	const context = canvasBackground.getContext('2d');
+
+
+
+	//the almighty game object containing all the methods and properties that propel the game forward, into oblivion
+	const game = {
+
+		allPlayers: [],
+
+		players: 0,
+
+		//gotta have it ya know
+		bindEvents() {
+			startButton.addEventListener('click', ()=> {
+				//the functions that really get this game going! ya know!
+			});
+		},
+
+		//coole background generation gradient thing to create a neato bg on a secondary canvas without interfering with the game canvas
+		bgGradient() {
+			let gradientHeight = canvasBackground.height / 10;
+			for (let i = 0; i < 11; i++) {
+				context.fillStyle = 'rgba(85, 15, 155,'+(i/10)+')';
+				context.fillRect(0, canvasBackground.height - (gradientHeight * i), canvasBackground.width, gradientHeight);
+			}
+		},
+
+		drawTerrain() {
+			//aligns the drawn lines so they're nice and crisp
+			let iStrokeWidth = 1;
+			let iTranslate = (iStrokeWidth % 2) / 2;
+			ctx.translate(iTranslate, iTranslate);
+
+			//set attributes for drawing the terrain
+			let slopeChange = 1;
+			let maxSlope = 3.5;
+			let slope = (Math.random() * slopeChange) - slopeChange;
+			let maxHeight = canvas.height;
+			let height = 300;
+			let valley = 400;
+			let peak = 250;
+
+
+			//loops across the width of the canvas, changing the height values to draw lines of different height from the base to create theterrain one line at a time
+			for (let i = 0; i < canvas.width; i++) {
+				height += slope;
+				slopeToAdd = (Math.random() * slopeChange) * 2 - slopeChange;
+				slope += slopeToAdd;
+				// console.log('slopetoadd: '+ slopeToAdd);
+				if (slope > maxSlope  || slope < -maxSlope) {
+					slope = (Math.random() * slopeChange) * 2 - slopeChange;
+				}
+				if (height > valley) {
+					slope *= -1;
+					height -= 2;
+					// console.log('first');
+				}
+				else if (height < peak) {
+					slope *= -1;
+					slope += 2;
+					// console.log('second');
+				}
+				// console.log(slope);
+
+
+				ctx.beginPath();
+				ctx.moveTo(i, maxHeight);
+				ctx.lineTo(i, height);
+				ctx.stroke();
+				// ctx.strokeStyle = 'lightseagreen';
+			}
+		},
+
+		//updates the divs outside the canvas with information about current tank properties (angle, power, which player, etc)
+		updateDisplay() {
+			let whoseTurn;
+			let arrPos;
+			for (let i = 0; i < allPlayers.length; i++) {
+				if (allPlayers[i].isTurn) {
+					whoseTurn = allPlayers[i];
+					arrPos = i + 1;
+				}
+			}
+			playerDisplay.innerHTML = 'It is player '+arrPos+'\'s turn.';
+			powerDisplay.innerHTML = 'Current power: '+whoseTurn.power+'.';
+			angleDisplay.innerHTML = 'Current angle: '+whoseTurn.angle+'.';
+		},
+
+		// gonna change this to requestAnimationFrame
+		frameLoop() {
+			updateDisplay();
+			setInterval(()=>{
+				frameLoop();
+			}, 100);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
 
 
 	//coole background gradient thing
-	function bgGradient() {
-		let gradientHeight = canvasBackground.height / 10;
-		for (let i = 0; i < 11; i++) {
-			context.fillStyle = 'rgba(85, 15, 155,'+(i/10)+')';
-			context.fillRect(0, canvasBackground.height - (gradientHeight * i), canvasBackground.width, gradientHeight);
-		}
-	}
+	// function bgGradient() {
+	// 	let gradientHeight = canvasBackground.height / 10;
+	// 	for (let i = 0; i < 11; i++) {
+	// 		context.fillStyle = 'rgba(85, 15, 155,'+(i/10)+')';
+	// 		context.fillRect(0, canvasBackground.height - (gradientHeight * i), canvasBackground.width, gradientHeight);
+	// 	}
+	// }
 
 
-	//draws the foreground upon which the tanks will be laid 
-	function drawTerrain() {
-		//aligns the drawn lines so they're nice and crisp
-		let iStrokeWidth = 1;
-		let iTranslate = (iStrokeWidth % 2) / 2;
-		ctx.translate(iTranslate, iTranslate);
+	// //draws the foreground upon which the tanks will be laid 
+	// function drawTerrain() {
+	// 	//aligns the drawn lines so they're nice and crisp
+	// 	let iStrokeWidth = 1;
+	// 	let iTranslate = (iStrokeWidth % 2) / 2;
+	// 	ctx.translate(iTranslate, iTranslate);
 
-		//set attributes for drawing the terrain
-		let slopeChange = 1;
-		let maxSlope = 3.5;
-		let slope = (Math.random() * slopeChange) - slopeChange;
-		let maxHeight = canvas.height;
-		let height = 300;
-		let valley = 400;
-		let peak = 250;
-
-
-		//loops across the width of the canvas, changing the height values to draw lines of different height from the base to create theterrain one line at a time
-		for (let i = 0; i < canvas.width; i++) {
-			height += slope;
-			slopeToAdd = (Math.random() * slopeChange) * 2 - slopeChange;
-			slope += slopeToAdd;
-			// console.log('slopetoadd: '+ slopeToAdd);
-			if (slope > maxSlope  || slope < -maxSlope) {
-				slope = (Math.random() * slopeChange) * 2 - slopeChange;
-			}
-			if (height > valley) {
-				slope *= -1;
-				height -= 2;
-				// console.log('first');
-			}
-			else if (height < peak) {
-				slope *= -1;
-				slope += 2;
-				// console.log('second');
-			}
-			// console.log(slope);
+	// 	//set attributes for drawing the terrain
+	// 	let slopeChange = 1;
+	// 	let maxSlope = 3.5;
+	// 	let slope = (Math.random() * slopeChange) - slopeChange;
+	// 	let maxHeight = canvas.height;
+	// 	let height = 300;
+	// 	let valley = 400;
+	// 	let peak = 250;
 
 
-			ctx.beginPath();
-			ctx.moveTo(i, maxHeight);
-			ctx.lineTo(i, height);
-			ctx.stroke();
-			// ctx.strokeStyle = 'lightseagreen';
-		}
-	}
+	// 	//loops across the width of the canvas, changing the height values to draw lines of different height from the base to create theterrain one line at a time
+	// 	for (let i = 0; i < canvas.width; i++) {
+	// 		height += slope;
+	// 		slopeToAdd = (Math.random() * slopeChange) * 2 - slopeChange;
+	// 		slope += slopeToAdd;
+	// 		// console.log('slopetoadd: '+ slopeToAdd);
+	// 		if (slope > maxSlope  || slope < -maxSlope) {
+	// 			slope = (Math.random() * slopeChange) * 2 - slopeChange;
+	// 		}
+	// 		if (height > valley) {
+	// 			slope *= -1;
+	// 			height -= 2;
+	// 			// console.log('first');
+	// 		}
+	// 		else if (height < peak) {
+	// 			slope *= -1;
+	// 			slope += 2;
+	// 			// console.log('second');
+	// 		}
+	// 		// console.log(slope);
+
+
+	// 		ctx.beginPath();
+	// 		ctx.moveTo(i, maxHeight);
+	// 		ctx.lineTo(i, height);
+	// 		ctx.stroke();
+	// 		// ctx.strokeStyle = 'lightseagreen';
+	// 	}
+	// }
 
 
 
@@ -104,38 +209,38 @@ const scorch = () => {
 		}
 	}
 
-	//updates the divs below the canvas with info about whose turn it is and their tank properties
-	function updateDisplay() {
-		let whoseTurn;
-		let arrPos;
-		for (let i = 0; i < allPlayers.length; i++) {
-			if (allPlayers[i].isTurn) {
-				whoseTurn = allPlayers[i];
-				arrPos = i + 1;
-			}
-		}
-		playerDisplay.innerHTML = 'It is player '+arrPos+'\'s turn.';
-		powerDisplay.innerHTML = 'Current power: '+whoseTurn.power+'.';
-		angleDisplay.innerHTML = 'Current angle: '+whoseTurn.angle+'.';
-	}
+	// //updates the divs below the canvas with info about whose turn it is and their tank properties
+	// function updateDisplay() {
+	// 	let whoseTurn;
+	// 	let arrPos;
+	// 	for (let i = 0; i < allPlayers.length; i++) {
+	// 		if (allPlayers[i].isTurn) {
+	// 			whoseTurn = allPlayers[i];
+	// 			arrPos = i + 1;
+	// 		}
+	// 	}
+	// 	playerDisplay.innerHTML = 'It is player '+arrPos+'\'s turn.';
+	// 	powerDisplay.innerHTML = 'Current power: '+whoseTurn.power+'.';
+	// 	angleDisplay.innerHTML = 'Current angle: '+whoseTurn.angle+'.';
+	// }
 
-	//loops the display display
-	function frameLoop() {
-		updateDisplay();
-		setInterval(()=>{
-			frameLoop();
-		}, 100);
-	}
+	// //loops the display display
+	// function frameLoop() {
+	// 	updateDisplay();
+	// 	setInterval(()=>{
+	// 		frameLoop();
+	// 	}, 100);
+	// }
 
 
-	//new tank class for building tanks, will refine
+	//new tank class for building tanks, refined some, still needs additional refining
 	class Tank {
 		constructor(xpos, ypos, power, angle, isTurn) {
 			// this.number = number;
 			this.xpos = xpos;
 			this.ypos = ypos;
 			this.power = 100; //arbitrary
-			this.angle = 90; //degress
+			this.angle = 90; //degreess
 			this.isTurn = false;
 		}
 		drawBody() {
@@ -182,8 +287,8 @@ const scorch = () => {
 		}
 	}
 
-	bgGradient();
-	drawTerrain();
+	game.bgGradient();
+	game.drawTerrain();
 
 
 	//this huge block of ugly let declarations is temporary, for testing purposes 
@@ -225,27 +330,8 @@ const scorch = () => {
 	allPlayers.push(tank3);
 
 
-	let test = ctx.getImageData(20, 20, 1, 1).data;
-	console.log(test);
-
-
-	// //testing bitmap stuff for canvas using getImageData etc
-	// let stop = 0;
-
-	// for (let i = 0; i < canvas.width; i++) {
-	// 	for (let j = 0; j < canvas.height; j ++) {
-	// 		let bitmapArr = ctx.getImageData(i,j,1,1).data;
-	// 		for (let k = 0; k < bitmapArr.length; k++) {
-	// 			if (bitmapArr[k] > 0) {
-	// 				console.log('it worked somehow', bitmapArr, i, j);
-	// 				stop++;
-	// 				if (stop > 50) {
-	// 					return;
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
+	// let test = ctx.getImageData(20, 20, 1, 1).data;
+	// console.log(test);
 
 
 	//WACKY LOOKIN keyboard input listener 
@@ -285,7 +371,7 @@ const scorch = () => {
 		}
 	});
 
-	frameLoop();
+	game.frameLoop();
 
 }
 
