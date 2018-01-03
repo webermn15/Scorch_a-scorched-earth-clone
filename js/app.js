506f677f643fee711649336fc75e83ba659fce43
Michael Weber <webermn15@gmail.com>
2018-01-03 12:43:31 -0600

    everything (so far) has filled in the game object now, fixed a bug with the buttons

--- js/app.js
+++ js/app.js
@@ -11,7 +11,9 @@ const scorch = () => {
 	const playerDisplay = document.getElementById('turn');
 	const powerDisplay = document.getElementById('power');
 	const angleDisplay = document.getElementById('angle');
-	const startButton = document.getElementById('START-ER-UP');
+	const twoButton = document.getElementById('2players');
+	const threeButton = document.getElementById('3players');
+	const fourButton = document.getElementById('4players');
 
 	//canvas
 	const canvas = document.getElementById('game-board');
@@ -28,12 +30,21 @@ const scorch = () => {
 
 		allPlayers: [],
 
-		players: 3,
+		players: 0,
 
 		//gotta have it ya know
 		bindEvents() {
-			startButton.addEventListener('click', ()=> {
-				//the functions that really get this game going! ya know!
+			twoButton.addEventListener('click', ()=> {
+				game.players = 2;
+				game.placeTank(2);
+			});
+			threeButton.addEventListener('click', ()=> {
+				game.players = 3;
+				game.placeTank(3);
+			});
+			fourButton.addEventListener('click', ()=> {
+				game.players = 4;
+				game.placeTank(4);
 			});
 		},
 
@@ -46,6 +57,7 @@ const scorch = () => {
 			}
 		},
 
+		//randomly generates a foreground upon which the tanks shall be laid
 		drawTerrain() {
 			//aligns the drawn lines so they're nice and crisp
 			let iStrokeWidth = 1;
@@ -215,132 +227,8 @@ const scorch = () => {
 
 
 
-
-	//coole background gradient thing
-	// function bgGradient() {
-	// 	let gradientHeight = canvasBackground.height / 10;
-	// 	for (let i = 0; i < 11; i++) {
-	// 		context.fillStyle = 'rgba(85, 15, 155,'+(i/10)+')';
-	// 		context.fillRect(0, canvasBackground.height - (gradientHeight * i), canvasBackground.width, gradientHeight);
-	// 	}
-	// }
-
-
-	// //draws the foreground upon which the tanks will be laid 
-	// function drawTerrain() {
-	// 	//aligns the drawn lines so they're nice and crisp
-	// 	let iStrokeWidth = 1;
-	// 	let iTranslate = (iStrokeWidth % 2) / 2;
-	// 	ctx.translate(iTranslate, iTranslate);
-
-	// 	//set attributes for drawing the terrain
-	// 	let slopeChange = 1;
-	// 	let maxSlope = 3.5;
-	// 	let slope = (Math.random() * slopeChange) - slopeChange;
-	// 	let maxHeight = canvas.height;
-	// 	let height = 300;
-	// 	let valley = 400;
-	// 	let peak = 250;
-
-
-	// 	//loops across the width of the canvas, changing the height values to draw lines of different height from the base to create theterrain one line at a time
-	// 	for (let i = 0; i < canvas.width; i++) {
-	// 		height += slope;
-	// 		slopeToAdd = (Math.random() * slopeChange) * 2 - slopeChange;
-	// 		slope += slopeToAdd;
-	// 		// console.log('slopetoadd: '+ slopeToAdd);
-	// 		if (slope > maxSlope  || slope < -maxSlope) {
-	// 			slope = (Math.random() * slopeChange) * 2 - slopeChange;
-	// 		}
-	// 		if (height > valley) {
-	// 			slope *= -1;
-	// 			height -= 2;
-	// 			// console.log('first');
-	// 		}
-	// 		else if (height < peak) {
-	// 			slope *= -1;
-	// 			slope += 2;
-	// 			// console.log('second');
-	// 		}
-	// 		// console.log(slope);
-
-
-	// 		ctx.beginPath();
-	// 		ctx.moveTo(i, maxHeight);
-	// 		ctx.lineTo(i, height);
-	// 		ctx.stroke();
-	// 		// ctx.strokeStyle = 'lightseagreen';
-	// 	}
-	// }
-
-
-
-
-	//this creates constraints for the generated tank based on player number, then generate a random x coordinate to place it at withint it's territory
-
-	function placeTankX(numOfPlayers,playerNumber) {
-
-		let terrWidth = canvas.width / numOfPlayers;
-		let rightBorder = playerNumber * terrWidth;
-
-		let leftBorder = rightBorder - (rightBorder / numOfPlayers);
-
-		return Math.floor(Math.random()*(rightBorder - leftBorder + 1) + leftBorder);
-	}
-
-	function placeTankY(xpos) {
-		for (let i = 0; i < canvas.height; i++) {
-			let checkCollision = ctx.getImageData(xpos, i, 1, 1).data;
-			for (let j = 0; j < checkCollision.length; j++) {
-				if (checkCollision[j] > 0) {
-					return i;
-				}
-			}
-		}
-	}
-
-	// //updates the divs below the canvas with info about whose turn it is and their tank properties
-	// function updateDisplay() {
-	// 	let whoseTurn;
-	// 	let arrPos;
-	// 	for (let i = 0; i < allPlayers.length; i++) {
-	// 		if (allPlayers[i].isTurn) {
-	// 			whoseTurn = allPlayers[i];
-	// 			arrPos = i + 1;
-	// 		}
-	// 	}
-	// 	playerDisplay.innerHTML = 'It is player '+arrPos+'\'s turn.';
-	// 	powerDisplay.innerHTML = 'Current power: '+whoseTurn.power+'.';
-	// 	angleDisplay.innerHTML = 'Current angle: '+whoseTurn.angle+'.';
-	// }
-
-	// //loops the display display
-	// function frameLoop() {
-	// 	updateDisplay();
-	// 	setInterval(()=>{
-	// 		frameLoop();
-	// 	}, 100);
-	// }
-
-
-
 	game.bgGradient();
 	game.drawTerrain();
-
-
-	// //this huge block of ugly let declarations is temporary, for testing purposes 
-
-	// let xTank = placeTankX(3,1);
-
-	// let yTank = placeTankY(xTank);
-
-	// tank1 = new Tank(xTank,yTank);
-
-
-	// tank1.drawBody();
-	// tank1.drawCannon();
-	// allPlayers.push(tank1);
-	// tank1.isTurn = true;
 
 
 	//WACKY LOOKIN keyboard input listener 
@@ -380,7 +268,8 @@ const scorch = () => {
 		}
 	});
 
-	game.placeTank(game.players);
+	game.bindEvents();
+	// game.placeTank(game.players);
 	// game.frameLoop();
 
 }
